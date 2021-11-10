9d99650a7c98f13fda073b1cf12388213e73737c
Edson Cea <ledzaran3@gmail.com>
2021-11-09 22:09:54 -0300

    trabalho-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,56 @@
+
+
+/* questão 1 interpretação
+console a vai retornar 10
+console b vai retornar 10 5 */
+
+/* questão 2 interpretação
+console vai retornar 10 10 10 */
+
+/* Questão 3 interpretação
+
+let horasTrabalhadas = 7
+let valorRecebidoDiario = 10 */
+
+
+/* Questão 1
+
+const nome = prompt("Qual o seu Nome");
+let idade = prompt("Qual sua idade?");
+
+console.log("Meu nome é" ,nome ,"Eu tenho" ,idade ,"Anos");
+console.log(typeof nome);
+console.log(typeof idade); */
+
+
+/* Questão 2
+
+const resposta1 = "sim"
+const resposta2 = "nao?"
+const resposta3 = "sim"
+
+const pergunta1 = prompt("Você gosta de festas?")
+const pergunta2 = prompt("Você tem cigarro?")
+const pergunta3 = prompt("Você dorme tarde?")
+
+console.log(pergunta1 + " - " + resposta1);
+console.log(pergunta2 + " - " + resposta2);
+console.log(pergunta3 + " - " + resposta3); */
+
+
+
+
+
+
+/* Desafio
+
+const primeiroNumero = prompt("Digite o primeiro número!") 
+const segundoNumero = prompt("Digite o segundo numero!") 
+
+let somaNumeros = Number(primeiroNumero)+Number(segundoNumero) 
+let multiplicaNumeros = primeiroNumero*segundoNumero 
+
+console.log("O primeiro número somado ao segundo número resulta em :", somaNumeros) 
+console.log("O primeiro número multiplicado pelo segundo número resulta em :", multiplicaNumeros) */
+
+

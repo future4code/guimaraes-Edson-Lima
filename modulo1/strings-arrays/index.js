c3835aae56d651298c2193d414497c5136551f45
Edson Cea <ledzaran3@gmail.com>
2021-11-17 22:37:40 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,60 @@
+// Questão 1 Interpretação
+
+// a - undefined 
+//   b - null
+ //  c - 11
+ //  d - 3
+ //  e - 11
+ //  f - 9
+
+// Questão 2 interpretação 
+
+// O valor será 27
+
+
+/* Questão 1 código 
+
+const nome = prompt("Qual é o seu nome?")
+const email = prompt("Qual é seu Email?")
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso, Seja bem-vinda(o) ${nome}.`)
+
+
+2----
+
+let comidasPreferidas = ['Torta Alemã', 'Filé Mignon', 'Açai', 'Escalopinho ao Molho', 'Churrasco'];
+console.log(comidasPreferidas[0]);
+console.log(comidasPreferidas[1]);
+console.log(comidasPreferidas[2]);
+console.log(comidasPreferidas[3]);
+console.log(comidasPreferidas[4]);
+
+
+comidasPreferidas[3] = prompt('Qual é a sua comida favorita?');
+console.log(comidasPreferidas);
+
+3-----
+
+const listaDeTarefas = []
+const tarefa1 = prompt("Diga a primeira tarefa");
+const tarefa2 = prompt("Diga a primeira tarefa");
+const tarefa3 = prompt("Diga a primeira tarefa");
+
+
+listaDeTarefas.push(tarefa1);
+listaDeTarefas.push(tarefa2);
+listaDeTaredas.push(tarefa3);
+
+
+console.log(tarefa1,tarefa2,tarefa3);
+
+console.log(listaDeTarefas);
+
+const indice = prompt("Digite um indice 0, 1 ou 2");
+
+const indiceNumero = Number(indice);
+
+console.log(indiceNumero)
+
+listaDeTarefas.splice(indiceNumero, 1) */
+

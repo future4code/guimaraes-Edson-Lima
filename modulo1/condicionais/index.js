42005ed70dfb91406e476ef4f0db7aa18897bbc5
Edson Cea <ledzaran3@gmail.com>
2021-11-25 21:18:38 -0300

    trabalho-condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,82 @@
+//exercício 1 interpretação
+
+//1. pede para o usuário digitar um número caso o resultado divisivel por 2 for
+//igual a 0 recebe mensagem passou no teste. 
+//2. zero
+//3. qualquer número diferente de zero
+
+//exercício 2 interpretação
+
+//1. ele mostra os preços de frutas de acordo com a fruta escolhida 
+//2. O preço da fruta maça  é  R$ 2.25
+//3. vai informar o preço de 5 que está no defalt no lugar da pêra
+
+//exercício 3 interpretação
+
+//1. pedindo um número ao usuário pelo prompt
+//2. se for 10 recebe a mensagem -10 não aparece nada
+//3. talvez seria melhor usar um else no lugar do let
+
+//exercício 1 código
+
+/* const respostaUsuario = prompt("Qual é sua idade?")
+const numeroUsuario = Number(respostaUsuario)
+
+if (numeroUsuario >= 18) {
+  console.log("Você pode dirigir")
+} else {
+    console.log("Você não pode dirigir")
+} */
+
+//exercício 2 código
+
+/* const turnosEstudantes = prompt('Diga qual dos turnos você estuda, M para matutino ou V para vespertino ou N para noturno')
+
+if (turnosEstudantes === "M") {
+    console.log("Bom dia!")
+} else if (turnosEstudantes === "V") {
+    console.log("Boa tarde!")
+}else if (turnosEstudantes === "N") {
+    console.log("Boa noite")
+} else {
+    console.log("Digite corretamente")
+} */
+
+//exercício 3 código
+
+/* let turnosAlunos = prompt("Diga qual turno você estuda, matutino, vespertino ou noturno")
+let turno
+switch (turnosAlunos) {
+  case "matutino":
+    turno = "Bom Dia!"
+    break;
+  case "vespertino":
+    turno = "Boa Tarde!"
+    break;
+  case "noturno":
+    turno = "Boa Noite!"
+    break;
+  
+  default:
+    turno = "Digite corretamente"
+    break;
+}
+console.log(`Seu turno é, ${turnosAlunos}, ${turno}`) */
+
+
+//exercício 4 código
+
+/* let generoFilme = prompt("Qual o genero do filme você que vai assistir?")
+let valorFilme = Number(prompt("Qual o valor do ingresso?"))
+
+ 
+    if (generoFilme && "fantasia"){
+         console.log("Bom filme")
+
+    }else if (valorFilme <= 15){  
+        console.log("Bom filme")
+    }
+    else { console.log("Escolha outro filme" )
+        } */
+
+     

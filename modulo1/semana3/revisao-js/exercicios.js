a867d1d76e67e23985896d8d1ec3951970249fcc
Edson Cea <ledzaran3@gmail.com>
2021-12-09 19:52:32 -0300

    revisao-js-exercicio4

--- modulo1/semana3/revisao-js/exercicios.js
+++ modulo1/semana3/revisao-js/exercicios.js
@@ -23,7 +23,15 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+  let arrayPares = []
+  for (i=0;i < array.length;i++)
+  { 
+     if (array[i] % 2 === 0)
+     {
+        arrayPares.push(array[i])
+     }
+   }
+   return arrayPares
 }
 
 // EXERCÍCIO 05

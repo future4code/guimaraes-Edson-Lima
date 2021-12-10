1e10525027eb116bbd5ea1a958309dc17658b882
Edson Cea <ledzaran3@gmail.com>
2021-12-09 22:31:21 -0300

    revisao-js-exercicio11

--- modulo1/semana3/revisao-js/exercicios.js
+++ modulo1/semana3/revisao-js/exercicios.js
@@ -138,6 +138,7 @@ function retornaSegundoMaiorESegundoMenor(array) {
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
    
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(', ')}.`
 }
 
 // EXERCÍCIO 12

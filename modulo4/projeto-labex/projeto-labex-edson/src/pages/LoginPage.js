ef657c6ca642d7d2e784aeefd62fa8ee9d6c2a70
Edson Cea <ledzaran3@gmail.com>
2022-04-05 21:55:07 -0300

    parte3

--- modulo4/projeto-labex/projeto-labex-edson/src/pages/LoginPage.js
+++ modulo4/projeto-labex/projeto-labex-edson/src/pages/LoginPage.js
@@ -1,6 +1,9 @@
-import react from "react";
+import axios from "axios";
+import react, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default ()=>{
+    const [email,setEmail]=useState('')
+    const [senha,setSenha]=useState('')
     const navigate=useNavigate()
     const goToBack=()=>{
         navigate('/')
@@ -8,13 +11,36 @@ export default ()=>{
     const goToAdmHome=()=>{
         navigate('/admhome')
     }
+    const handleEmail=(event)=>{
+        setEmail(event.target.value)
+
+    }
+    const handleSenha=(event)=>{
+        setSenha(event.target.value)
+
+    }
+    const login=()=>{
+        console.log(email,senha)
+        const body={
+            "email": email,
+            "password": senha
+        }
+        const url="https://us-central1-labenu-apis.cloudfunctions.net/labeX/Leonardo-Santos-guimaraes/login"
+        axios.post(url,body).then((response)=>{
+
+            localStorage.setItem("token",response.data.token)
+            goToAdmHome()
+        }).catch((error)=>{
+            alert(error.response.data.message)
+        })
+    }
     return(
         <>
         <h2> Login</h2>
-        <input  placeholder="Login"/>
-        <input placeholder="senha"/>
+        <input  placeholder="E-mail" value={email} onChange={handleEmail}/>
+        <input placeholder="senha" type="password" value={senha} onChange={handleSenha}/>
         <button onClick={goToBack}> Voltar</button>
-        <button onClick={goToAdmHome}> Entrar </button>
+        <button onClick={login}> Entrar </button>
         </>
     )
 }

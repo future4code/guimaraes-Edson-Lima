2dccd915f4598cf52a43c494261e784c6d7e32dc
Edson Cea <ledzaran3@gmail.com>
2022-10-18 23:55:46 -0300

    inicio case personare

---
+++ cases/personare/src/pages/Home/index.js
@@ -0,0 +1,19 @@
+
+function Home() {
+
+return(
+
+    <div>
+    <h1>Tarot</h1>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    </div>
+
+    )
+
+}
+
+export default Home;
